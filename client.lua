5265a4c092598603350459c5106ca4a47dcf052f
izMystic <mystic@izmystic.dev>
2021-04-16 03:26:19 -0400

    Add RPC Buttons

--- client.lua
+++ client.lua
@@ -5,6 +5,8 @@ Citizen.CreateThread(function()
     SetDiscordAppId(DiscordAppId)
     SetDiscordRichPresenceAsset(DiscordAppAsset)
     SetRichPresence("DISCORD_LINK_HERE")
+    SetDiscordRichPresenceAction(0, "Button 1 Text", "Button 1 link")
+    SetDiscordRichPresenceAction(1, "Button 2 Text", "Button 2 Link")
     while true do
         Citizen.Wait(2500)
     end
